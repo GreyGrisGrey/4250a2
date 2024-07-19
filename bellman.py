476c1efd7e70c786b2cc9f362612ca67eafb1e4c
greygrisgrey <myusernamesuckssorry@gmail.com>
2024-07-18 23:48:54 -0230

    Working on the bellman

--- bellman.py
+++ bellman.py
@@ -1,10 +1,15 @@
 from numpy import linalg
+
+# This code is a mess, but I have spent many hours at it and it is still a mess so it will remain so.
+# The system of bellman functions is stored in systems.txt for cleanliness
+# I do not know if this works in the final version as I am still working on it now
 equations = []
 data = open("systems.txt", "r")
 blockA = data.readline()
 dataA = blockA.split(" ")
 dataA[24] = -1
 data.readline()
+# Takes the individual equations from the systems file and processes them
 for i in range(25):
     equation = []
     newBlock = data.readline()
@@ -21,6 +26,8 @@ for i in range(25):
     equations.append(equation)
 for i in range(25):
     dataA[i] = -float(dataA[i])
+
+# Uses the linear algorithm solver from numpy to solve the system of equations
 x = linalg.solve(equations, dataA)
 for i in range(5):
     newLine = []
